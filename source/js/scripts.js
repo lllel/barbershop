6cdabd559800f3c1e26071ebfe21b6688da5a4d8
Andrey <andrey_9112@mail.ru>
2018-04-20 19:28:59 +0300

    Доработал мобильное меню

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -32,10 +32,13 @@ navMain.classList.remove('nav-main--active');
 class ModalMenu {
   constructor(options) {
     this.elem = options.elem;
+    this.item = this.elem.querySelectorAll('.nav-main__item');
     this.button = this.elem.querySelector('.nav-main__open-menu');
   }
 
   openMenu() {
+    this.items = document.querySelector('.nav-main__items');
+
     this.elem.classList.remove('nav-main--hidden');
     this.elem.classList.add('nav-main--active');
     this.elem.classList.add('animation-play');
@@ -321,3 +324,65 @@ const loginMenu = new LoginMenu({
 });
 
 loginMenu.init();
+
+// PAGE-TOP-MOVE
+class PageMove {
+  constructor(options) {
+    this.elem = options.elem;
+    this.countSteps = 0;
+  }
+
+  pageScroll() {
+    this.elemCoords = this.elem.getBoundingClientRect().top + pageYOffset;
+
+    if (!this.elem.classList.contains('page-top--active') && this.elemCoords > 975) {
+      this.elem.classList.add('page-top--active');
+    }
+
+    if (this.elem.classList.contains('page-top--active') && this.elemCoords < 975) {
+      this.elem.classList.remove('page-top--active');
+    }
+  }
+
+  onElemClick() {
+    let intervalId = setInterval(() => {
+      if (document.documentElement.scrollTop === 0) {
+        clearInterval(intervalId);
+
+        this.countSteps = 0;
+      }
+
+      if (this.countSteps <= 30) {
+        window.scrollBy(0, -5);
+      }
+
+      if (this.countSteps <= 50) {
+        window.scrollBy(0, -20);
+      }
+
+      if (this.countSteps <= 70) {
+        window.scrollBy(0, -40);
+      }
+
+      if (this.countSteps > 100) {
+        window.scrollBy(0, -60);
+      }
+    }, 15);
+  }
+
+  init() {
+    this.elem.addEventListener('click', () => {
+      this.onElemClick();
+    });
+
+    window.addEventListener('scroll', () => {
+      this.pageScroll();
+    });
+  }
+}
+
+const pageMove = new PageMove({
+  elem: document.querySelector('.page-top')
+});
+
+pageMove.init();
